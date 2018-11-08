4ede063a7c7a39db121d6c3a925e6f1edd523b31
Dave Friederich <friederichd@stifel.com>
2018-11-08 16:27:19 -0600

    added new ami created user_data function added aws_security_group resource

--- main.tf
+++ main.tf
@@ -3,11 +3,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "webserver1" {
-  ami = "ami-0552e3455b9bc8d50"
+  ami = "ami-40d28157"
   instance_type = "t2.micro"
   
+  user_data = <<-EOF
+      #!/bin/bash
+      echo "Hello, World" > index.html
+      nohup busybox httpd -f -p 8080 &
+      EOF
+
   tags {
     Name = "terraform-webserver1"
   }
 }
 
+resource "aws_security_group" "webservers" {
+  name    ="webservers"
+  description = "Allows all inbound traffic"
+  
+  ingress {
+    
+  }
+
+}
