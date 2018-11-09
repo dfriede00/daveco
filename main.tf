37e90671457a0b8fdeb0360fcb516413212c57b7
Friederich <friederichd@stifel.com>
2018-11-08 20:31:55 -0600

    modified security group sg_webservers changed ami to ubuntu16.04 created port variable added output values

--- main.tf
+++ main.tf
@@ -3,13 +3,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "webserver1" {
-  ami = "ami-40d28157"
+  ami = "ami-3ec9fd5b"
   instance_type = "t2.micro"
-  
+  vpc_security_group_ids = ["${aws_security_group.sg_webservers.id}"]
+
   user_data = <<-EOF
       #!/bin/bash
       echo "Hello, World" > index.html
-      nohup busybox httpd -f -p 8080 &
+      nohup busybox httpd -f -p "${var.http_port}" &
       EOF
 
   tags {
@@ -17,12 +18,38 @@ resource "aws_instance" "webserver1" {
   }
 }
 
-resource "aws_security_group" "webservers" {
-  name    ="webservers"
-  description = "Allows all inbound traffic"
-  
+resource "aws_security_group" "sg_webservers" {
+  name = "allow_http"
+  description = "Allow http traffic"
+
   ingress {
-    
+    from_port = "${var.http_port}"
+    to_port = "${var.http_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # egress {
+  #   from_port = 0
+  #   to_port = 8080
+  #   protocol = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+
+    tags {
+      Name = "tf_sg_allow_http"
+    }
+}
+variable "http_port" {
+  description = "http port of 8080"
+  default = "8080"
+}
+
+output "public_ip" {
+  value = "${aws_instance.webserver1.public_ip}"
+}
+output "Webserver1_ID" {
+  value = "${aws_instance.webserver1.id}"
+}
+output "not sure" {
+  value = "${aws_instance.webserver1.vpc_security_group_ids}"
 }
